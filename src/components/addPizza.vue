8792cf915899f5290a257ab597e24aa40ffcb67e
robertobirijnr <robertobirijnr@hotmail.com>
2020-11-03 23:02:10 +0000

    newPizzar form

---
+++ src/components/addPizza.vue
@@ -0,0 +1,75 @@
+<template>
+    <form>
+        <h3>Add new pizza:</h3>
+        <div class="form-group row">
+            <label for="Name" class="col-sm-3">Name</label>
+            <div class="col-sm-9">
+                <input type="text" v-model="newPizza.name" class="form-control">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="description" class="col-sm-3">Description</label>
+            <div class="col-sm-9">
+                <textarea type="text" v-model="newPizza.description" class="form-control"> </textarea>
+            </div>
+        </div>
+        <p><strong>Option 1:</strong></p>
+        <div class="form-group row">
+            <label for="size" class="col-sm-3">Size (")</label>
+            <div class="col-sm-9">
+                <input type="text" v-model="newPizza.options[0].size" class="form-control">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="price" class="col-sm-3">Price</label>
+            <div class="col-sm-9">
+                <input type="text" v-model="newPizza.options[0].price" class="form-control">
+            </div>
+        </div>
+        <p><strong>Option 2:</strong></p>
+        <div class="form-group row">
+            <label for="size" class="col-sm-3">Size (")</label>
+            <div class="col-sm-9">
+                <input type="text" v-model="newPizza.options[1].size" class="form-control">
+            </div>
+        </div>
+        <div class="form-group row">
+            <label for="price" class="col-sm-3">Price</label>
+            <div class="col-sm-9">
+                <input type="text" v-model="newPizza.options[1].price" class="form-control">
+            </div>
+        </div>
+        <div class="form-group row">
+            <button @click="addMenuItem" type="button" class="btn btn-info btn-block">Add</button>
+        </div>
+    </form>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                newPizza:{
+                    'name':"Margherita",
+                    'description':"  Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum dignissimos similique odit necessitatibus hic doloremque, minima nostrum reiciendis repudiandae a quos iste officia, maiores odio! Temporibus excepturi sed laborum a.",
+                    'options':[{
+                        'size':9,
+                        'price':6.95
+                    },{
+                        'size':12,
+                        'price':10.95
+                    }]
+                }
+            }
+        },
+        methods:{
+            addMenuItem(){
+                console.log(this.newPizza)
+            }
+        }
+    }
+</script>
+
+<style  scoped>
+
+</style>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="container">
-       <div class="col-sm-12 col-md-6">
-           
+       <div class="row pt-5">
+           <div class="col-sm-12 col-md-6">
+           <AddPizza/>
        </div>
        <div class="col-sm-12 col-md-6">
            <h3>Menu:</h3>
-           <table class="table table-hover">
-               <thead class="thead-default">
+           <table class="table table-hover table-bordered">
+               <thead class="thead-dark">
                    <tr>
                        <th>Item</th>
                        <th>Remove From menu</th>
@@ -20,12 +21,44 @@
                </tbody>
            </table>
        </div>
-    </div>
+       </div>
+       <div class="row">
+           <div class="col-sm-12 pt-4">
+               <h3>Current Order</h3>
+                <table class="table table-sm table-bordered">
+               <thead class="thead-dark">
+                   <tr>
+                       <th>Item</th>
+                       <th>Size</th>
+                       <th>Qunatity</th>
+                       <th>Price</th>
+                   </tr>
+               </thead>
+               <tbody>
+                   <div class="order-number">
+                       <strong><em>Order Number: 1</em></strong>
+                       <button class="btn btn-outline-danger btn-sm">X</button>
+                   </div>
+                   <tr>
+                       <td>Margherita</td>
+                       <td>9"</td>
+                       <td>1</td>
+                       <td>6.95</td>
+                   </tr>
+               </tbody>
+           </table>
+       </div>
+           </div>
+       </div>
+    
 </template>
 
 <script>
+import AddPizza from '@/components/addPizza'
     export default {
-        
+       components:{
+           AddPizza
+       } 
     }
 </script>
 
